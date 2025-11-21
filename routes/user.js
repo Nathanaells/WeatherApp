2bfed809f10c26287001f864232f5018f40ff2b7
Nathanaells <nathanaelsinaga27@gmail.com>
2025-11-21 10:25:22 +0700

    fixing server

--- routes/user.js
+++
@@ -1,8 +0,0 @@
-const ControllersUser = require("../controller/controllerUser");
-const router = require("express").Router();
-
-router.post("/register", ControllersUser.register);
-router.post("/login", ControllersUser.login);
-router.post("/login/google", ControllersUser.loginGoogle);
-
-module.exports = router;
